925d5f9708e1f60b8f83436a4bc9736f2a49cd1e
Magno <jmagno.dutra@hotmail.com>
2025-01-09 22:23:01 -0300

    exercicio 3 completo

--- Exercicio_3/Program.cs
+++ Exercicio_3/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System.Text.Json;
-using System.Text.Json.Serialization;
 using Exercicio_3.Data;
 
 string filePath = "Data/dados.json";
@@ -7,7 +6,42 @@ string filePath = "Data/dados.json";
 string json = File.ReadAllText(filePath);
 var faturamentoList = JsonSerializer.Deserialize<List<Faturamento>>(json);
 
-if (faturamentoList == null || faturamentoList.Count == 0)
+void CalcularFaturamento()
 {
-  System.Console.WriteLine("Não há dados disponíveis");
+
+  if (faturamentoList == null || faturamentoList.Count == 0)
+  {
+    Console.WriteLine("Não há dados disponíveis");
+    return;
+  }
+
+  double menorValor = faturamentoList[0].valor;
+  double maiorValor = faturamentoList[0].valor;
+  double soma = 0;
+
+  foreach (var dia in faturamentoList)
+  {
+    if (dia.valor < menorValor)
+      menorValor = dia.valor;
+
+    if (dia.valor > maiorValor)
+      maiorValor = dia.valor;
+
+    soma += dia.valor;
+  }
+
+  double media = soma / faturamentoList.Count;
+
+  int diasAcimaMedia = 0;
+
+  foreach (var dia in faturamentoList)
+  {
+    if (dia.valor > media)
+      diasAcimaMedia++;
+  }
+
+  Console.WriteLine($"O menor valor de faturamento do mês é: {menorValor}");
+  Console.WriteLine($"O maior valor de faturamento do mês é: {maiorValor}");
+  System.Console.WriteLine($"Teve {diasAcimaMedia} dias acima da média.");
+
 }
