83084c3b93e05f0103a2e1444832e0fbed0cb5ee
Magno <jmagno.dutra@hotmail.com>
2025-01-09 22:32:07 -0300

    exercicio 5 resolvido

---
+++ Exercicio_5/Program.cs
@@ -0,0 +1,18 @@
+﻿string ReversedString(string str)
+{
+  string reversed = "";
+
+  for (int i = str.Length - 1; i >= 0; i--)
+  {
+    reversed += str[i];
+  }
+
+  return reversed;
+}
+
+System.Console.WriteLine("Digite um texto: ");
+
+string? input = Console.ReadLine();
+string reversedString = ReversedString(input);
+
+System.Console.WriteLine($"String ao contrário: {reversedString}");
